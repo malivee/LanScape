c84c9e03d41badb76b279e8fdc21987017556a4d
malivee <muhalif7123@gmail.com>
2026-08-20 10:42:09 +0700

    initial commit

---
+++ LanScape/LanScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanScapeApp.swift
+//  LanScape
+//
+//  Created by Muhammad Alief Rahman Fardillah on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LanScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
